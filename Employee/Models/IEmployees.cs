5c50b234e6a1933a92ce908e7939b5008b70ccf5
Nanda yudha pratama <nanda.yudha.ft18@mail.umy.ac.id>
2020-10-19 11:46:36 +0700

    Add project files.

---
+++ Employee/Models/IEmployees.cs
@@ -0,0 +1,11 @@
+﻿namespace Employee.Models
+{
+    public interface IEmployees
+    {
+        string EmpCode { get; set; }
+        int EmployeeId { get; set; }
+        string FullName { get; set; }
+        string OfficeLocation { get; set; }
+        string Position { get; set; }
+    }
+}
